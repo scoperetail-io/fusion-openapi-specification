240ecff6675c874b560e4f5eed7f7fa347769abb
Ravi Zadpide <ravi.zadpide@scoperetail.com>
2021-05-05 11:50:07 +0530

    Initial commit

---
+++ src/test/java/com/scoperetail/fusion/OpenapiSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoperetail.fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
